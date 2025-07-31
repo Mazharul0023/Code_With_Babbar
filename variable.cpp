e125327b502b188c5e9cf886d329d83755058182
mazharul haque Leon <mazharhaqueaiub@gmail.com>
2025-07-31 23:54:14 +0600

    Refactor hello world program and add variable demonstration

---
+++ variable.cpp
@@ -0,0 +1,29 @@
+#include<iostream> 
+using namespace std;
+int main(){
+/*
+int a = 10;
+cout<<a<<endl;
+
+char b ='a';
+cout<<b<<endl;
+
+bool d = true;
+cout<<d<<endl;
+
+float c = 13.5;
+cout<<c<<endl;
+
+
+
+int a ='a';
+cout<<a<<endl;
+*/
+
+char al1="1234567";
+cout<<al1<<endl;
+
+
+
+return 0;
+}
